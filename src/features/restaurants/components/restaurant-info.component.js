ddf014bfdda59a33de4a11bc51742c0775900e18
Kantaaa <kantasitms1@outlook.com>
2022-07-09 21:36:03 +0200

    fixing all the mess with theme and my horrible git routine

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Text, StyleSheet } from 'react-native';
 import { Card } from 'react-native-paper';
 
 const RCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) =>
+    props.theme.colors.bg.primary};
 `;
 const RCover = styled(Card.Cover)`
-  padding: 20px;
-  background-color: white;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) =>
+    props.theme.colors.bg.primary};
 `;
 
 const Title = styled.Text`
-  padding: 16px;
-  color: red;
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.text.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = 'Le Sandwiches',
+    name = 'Kantas Sandwiches',
     icon,
     photos = [
       'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg'
